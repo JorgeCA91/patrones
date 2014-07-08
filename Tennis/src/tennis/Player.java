c81daf11270bd180ff60904642a3a3b1775b5883
Jorge Arellanes <jo2409@hotmail.com>
2014-07-08 18:17:21 -0500

    Agregando Proyeco Tennis

---
+++ Tennis/src/tennis/Player.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tennis;
+
+/**
+ *
+ * @author jorge
+ */
+public class Player {
+    
+    public String nombre;
+    public int point;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+    
+}
