992a42621445d6b448ad1b6c2c1de884b5f47185
Jorge Arellanes <jo2409@hotmail.com>
2014-08-03 19:43:22 -0500

    Comenzando refactor del codigo del juego tennis

--- Tennis/src/tennis/Juego.java
+++ Tennis/src/tennis/Juego.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package tennis;
 
 /**
@@ -12,14 +6,44 @@ package tennis;
  */
 public class Juego {
     
-    public String score;
+    private Player player1;
+    private Player player2;
+    private String score;
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(Player player1) {
+        this.player1 = player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(Player player2) {
+        this.player2 = player2;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+    
+    public Juego(Player player1, Player player2) {
+        setPlayer1(player1);
+        setPlayer1(player2);
+    }
     
     public void point(Player jugador) {
         
     }
     
     public String score(Player uno, Player dos) {
-        
         return score;
     } 
 }
